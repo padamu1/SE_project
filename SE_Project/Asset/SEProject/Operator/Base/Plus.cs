3a9700cc7e268028ba443d44ea48399d69e69e42
김선오 <padamu1@naver.com>
2022-12-07 23:40:30 +0900

    operator 매니저 추가

---
+++ SE_Project/Asset/SEProject/Operator/Base/Plus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEProject.Operator.Base
+{
+    public class Plus : IOperator
+    {
+        public string Name { get => "+";}
+
+        public object Invoke(object[] param)
+        {
+            if(param.Length <= 1)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+
+            }
+            return param[0];
+        }
+    }
+}
---
+++ SE_Project/Asset/SEProject/Operator/OperatorManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEProject.Operator
+{
+    public class OperatorManager
+    {
+        private Dictionary<string, IOperator> operatorDic;
+        public OperatorManager()
+        {
+            operatorDic = new Dictionary<string, IOperator>();
+        }
+        public void SetOperator(IOperator operation)
+        {
+            if (operatorDic.ContainsKey(operation.Name))
+            {
+                Console.WriteLine("{0} already added", operation.Name);
+                return;
+            }
+            operatorDic[operation.Name] = operation;
+        }
+        public object Excute(string operatorName, object[] param)
+        {
+            if (operatorDic.ContainsKey(operatorName))
+            {
+                return operatorDic[operatorName].Invoke(param);
+            }
+            Console.WriteLine("{0} not exist", operatorName);
+            return null;
+        }
+    }
+
+}
